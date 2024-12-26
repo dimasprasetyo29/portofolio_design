5c245a628feb11de49b13d23870ef1b278c641e4
AhmadZidan0108 <muslikahasyadad@gmail.com>
2024-12-26 20:40:49 +0700

    portofolio_design

---
+++ portofolio_design/src/main/java/com/example/portofolio_design/portofolio_design/PortofolioDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.portofolio_design.portofolio_design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortofolioDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortofolioDesignApplication.class, args);
+	}
+
+}
---
+++ portofolio_design/src/test/java/com/example/portofolio_design/portofolio_design/PortofolioDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portofolio_design.portofolio_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortofolioDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
